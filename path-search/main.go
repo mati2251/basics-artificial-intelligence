9cbd66591e473c549c063bfee0a81a4f3aa6417f
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2024-02-27 07:51:03 +0100

    Added climb algorithm

--- path-search/main.go
+++ path-search/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"path-search/graph"
+	"path-search/heuristic"
 	"path-search/non-heuristic"
 )
 
 func main() {
 	root := graph.TestGraph()
 	graph.PrintGraph(root)
+  fmt.Println("Non-heuristic search")
   dfsKeys := non.DFSfirst(root)
   fmt.Println(dfsKeys)
   bfsKeys := non.BFS(root)
@@ -17,4 +19,7 @@ func main() {
   fmt.Println(idsKeys)
   ucsKeys := non.UCS(root)
   fmt.Println(ucsKeys)
+  fmt.Println("Heuristic search")
+  climbingKeys := heu.Climbing(root)
+  fmt.Println(climbingKeys)
 }
